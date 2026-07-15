af74d3616fb07c6218c1fce330e1ab6813773e33
Ritwikmondal2004 <ritwik8575@gmail.com>
2026-07-15 16:28:53 +0530

    update

---
+++ java oops/08.String/string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class string {
+    public static void main(String[]args){
+        String name="Ritwik";
+        name=name+"rubby";
+        System.out.println("name= "+name);
+
+        String s1="navin";
+        String s2="navin";
+        System.out.println(s1=s2); //mutibal and inmutible string (mutable change , unmutable unchange)
+    }
+}
---
+++ java oops/08.String/stringBuffer.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class stringBuffer {
+    public static void main(String[]args){
+        StringBuffer sb=new StringBuffer("Ritwik");
+    //	System.out.println(sb.length());
+    //		System.out.println(sb.capacity());
+		sb.append("Reddy");
+		System.out.println(sb);
+		
+    //	String str=sb.toString();
+		
+//		sb.deleteCharAt(2);
+//		sb.insert(0,"Java");
+//		sb.insert(6,"java");
+//		sb.setLength(30);
+		sb.ensureCapacity(100);
+		
+		System.out.println(sb);
+    }
+}
